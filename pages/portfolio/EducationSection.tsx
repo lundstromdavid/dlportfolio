078dc2e264d28a43e8daaa5d761364d559316107
David Lundström <david@lundstrom.eu>
2022-02-03 16:07:20 +0100

    Added outgoing links on the portfolio-education section, as I believe it helps with SEO - but I have to read more about that

--- pages/portfolio/EducationSection.tsx
+++ pages/portfolio/EducationSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { AppLink } from "../../components/link/AppLink";
 import { PortfolioSection } from "./PortfolioSection";
 
 interface Props {
@@ -47,12 +48,18 @@ const Heading = styled.h3`
 const SubHeading = styled.h4`
   grid-area: subheader;
   font-size: min(1rem, 3.4vw);
+  gap: 0.5rem;
+  display: flex;
 `;
 const Time = styled.span`
   font-style: italic;
 `;
 
-const Description = styled.p``;
+const StyledLink = styled.a`
+  color: var(--text-black);
+  font-size: inherit;
+  margin-bottom: auto;
+`;
 
 export const EducationSection = (props: Props) => {
   return (
@@ -64,16 +71,29 @@ export const EducationSection = (props: Props) => {
     >
       <Item>
         <Logo src="logo/coursera.png" width={150} height={150} />
-        <Heading>Foundations of User Experience (UX) Design</Heading>
+        <Heading>
+          <StyledLink
+            href="https://www.coursera.org/learn/foundations-user-experience-design"
+            target="_blank"
+          >
+            Foundations of User Experience (UX) Design
+          </StyledLink>
+        </Heading>
         <SubHeading>
-          coursera, <Time>2022 - present</Time>
+          <StyledLink href="https://www.coursera.org/" target="_blank">
+            coursera,
+          </StyledLink>{" "}
+          <Time>2022 - present</Time>
         </SubHeading>
       </Item>
       <Item>
         <Logo src="logo/uu.png" width={150} height={150} />
         <Heading>Computer Science</Heading>
         <SubHeading>
-          Uppsala University, <Time>2020 - 2022</Time>
+          <StyledLink href="https://www.uu.se/en/?languageId=1" target="_blank">
+            Uppsala University,
+          </StyledLink>
+          <Time>2020 - 2022</Time>
         </SubHeading>
       </Item>
     </PortfolioSection>
